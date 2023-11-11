c7e39e979f85702ef77f7a69cfe86fed2ef7941b
gilevet9 <141581673+gilevet9@users.noreply.github.com>
2023-11-11 16:15:43 +0600

    Initial commit

---
+++ src/main/java/org/top/springmvcex/MainController.java
@@ -0,0 +1,2 @@
+package org.top.springmvcex;public class MainController {
+}
---
+++ src/main/java/org/top/springmvcex/SpringMvcExApplication.java
@@ -0,0 +1,13 @@
+package org.top.springmvcex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcExApplication.class, args);
+	}
+
+}
