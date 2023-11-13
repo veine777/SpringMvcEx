d9f2cc354a603f06b455eb1cb238c453fccd2f2e
gilevet9 <141581673+gilevet9@users.noreply.github.com>
2023-11-13 20:17:07 +0600

    Readme add

--- src/main/java/org/top/springmvcex/MainController.java
+++ src/main/java/org/top/springmvcex/MainController.java
@@ -1,2 +1,50 @@
-package org.top.springmvcex;public class MainController {
+package org.top.springmvcex;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+public class MainController {
+    @GetMapping("")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("time")
+    public String time(Model model){
+        String currentTime = LocalDateTime.now().toString();
+        model.addAttribute("nowTime", currentTime);
+        return "time";
+    }
+    @GetMapping("info")
+    public String info(Model model){
+        //получение текущей даты
+        //текущая дата
+        LocalDate currentDate = LocalDate.now();
+        //формирование даты в формате дд мм гггг
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String formattedDate = currentDate.format(formatter);
+        model.addAttribute("formattedDate", formattedDate);
+
+        //получение ОС
+        String osName = System.getProperty("os.name");
+        model.addAttribute("osName", osName);
+
+        //получение osVersion
+        String osVersion = System.getProperty("os.version");
+        model.addAttribute("osVersion", osVersion);
+
+        //получение osArch
+        String osArch = System.getProperty("os.arch");
+        model.addAttribute("osArch", osArch);
+
+        return "info";
+    }
+
+
 }
--- src/main/java/org/top/springmvcex/SpringMvcExApplication.java
+++ src/main/java/org/top/springmvcex/SpringMvcExApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMvcExApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMvcExApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcExApplication.class, args);
+    }
 
 }
